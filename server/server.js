747db3fe35370a18a4d3ad828760139bc0842023
Kaveh Shahedi <kavehshahedi@gmail.com>
2024-10-21 20:41:30 -0400

    [UPDATE] Increase server's request size limitation

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 // Connect to MongoDB
 connectDB();
